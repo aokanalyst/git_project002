f80876cf0d027c484d8f74b6e611e1a9022b463e
AOKanalyst <aokanalyst@gmail.com>
2020-08-09 11:50:08 -0400

    This is fourt commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,9 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 	
 		System.out.println("This is the first changes from local master");
-		System.out.println("This is second commit from local master");
-		System.out.println("This is third commit from local master");
+		System.out.println("This is the second commit from local master");
+		System.out.println("This is the third commit from local master");
 		System.out.println("This is the first commit from remote repo");
+		System.out.println("This is the fourth commit from local master");
 		
 	}
 
